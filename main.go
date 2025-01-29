10b5e0b6b31f6a218f6d21ab6a9061bc7931802b
noodlecak-e <jnkrmg+github@gmail.com>
2025-01-29 07:06:59 +0530

    Get psql to run and connect to it

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/noodlecak-e/pix/internal/resources/event"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
+	dsn := "host=localhost user=pix_user password=pix_pass dbname=pix port=5432 sslmode=disable"
+	_, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("failed to connect database")
+	}
+
 	eventHandler := event.NewHandler()
 
 	r := gin.Default()
